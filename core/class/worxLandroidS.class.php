952a636bcd9369993b0e1f277bb1151e673158c5
sebsst <sebastien.siegrist@yahoo.fr>
2018-05-16 17:05:07 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -483,6 +483,8 @@ schedule: TimePeriod[];
         self::newInfo($elogic,'Planning/Saturday/Starttime',$json2_data->cfg->sc->d[6][0],'string',1);
   */
     }
+	  
+	  $elogic->save();
 	  $elogic->refreshWidget();
 	  
     // $this->refreshWidget();
