939384f7a305e1829ce3a58cb79f4b1be8143915
sebsst <sebastien.siegrist@yahoo.fr>
2018-05-17 21:24:51 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -760,7 +760,7 @@ log::add('worxLandroidS', 'debug', 'exception ' . $e );
 		
 	        $errorCode = $this->getCmd(null, 'errorCode');
 		$replace['#errorCode#'] = is_object($errorCode) ? $errorCode->execCmd() : '';
-		$replace['#errorColor#'] = 'green';
+		$replace['#errorColor#'] = 'darkgreen';
 		if($replace['#errorCode#'] != 0 ){$replace['#errorColor#'] = 'orange';}
 		
 		$replace['#errorID#'] = is_object($errorCode) ? $errorCode->getId() : '';
