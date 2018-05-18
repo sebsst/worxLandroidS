8c4ecc325cb3f2dcefcec5693fa7f49563c2f482
sebsst <sebastien.siegrist@yahoo.fr>
2018-05-18 21:23:10 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -655,7 +655,7 @@ schedule: TimePeriod[];
         $mosqId = config::byKey('mqtt_client_id', 'worxLandroidS'). '/' . $id . '/' . substr(md5(rand()), 0, 8);
         // FIXME: the static class variable $_client is not visible here as the current function
         // is not executed on the same thread as the deamon. So we do create a new client.
-        $client = new Mosquitto\Client(config::byKey('mqtt_client_id', 'worxLandroidS'));
+        $client = new Mosquitto\Client($mosqId);
         $client->setTlsCertificates($root_ca,$certfile,$pkeyfile,null);	  
 	$qos = '0';
 	$retain = '0';
@@ -690,8 +690,8 @@ while (true) {
 
         }catch(Mosquitto\Exception $e){
             //echo"{$e}" ;
-		log::add('worxLandroidS', 'debug', 'Publication du message ' . $e);
-               // return;
+		//log::add('worxLandroidS', 'debug', 'Publication du message ' . $e);
+                return;
         }
         sleep(2);
 }
