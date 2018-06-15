8d1915a11a624282e431ac43fc00e3c73df97a44
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-16 00:44:06 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -1,14 +1,5 @@
 #! /bin/bash
 
 echo "Début d'installation des dépendances"
-
-wget http://mosquitto.org/files/source/mosquitto-1.4.2.tar.gz
-tar zxvf mosquitto-1.4.2.tar.gz
-
-cd mosquitto-1.4.2
-make
-make install
-
-cp mosquitto.conf /etc/mosquitto
-
+sudo apt-get dist-upgrade
 echo "Fin installation des dépendances"
