5e85fc6cb6649a71e5a1bd5a05c410d90eca9790
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-16 00:44:40 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 echo "Début d'installation des dépendances"
-sudo apt-get dist-upgrade
+sudo apt-get -y dist-upgrade
+sudo apt-get install mosquitto mosquitto-clients
 echo "Fin installation des dépendances"
