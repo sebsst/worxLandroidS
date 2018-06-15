ab9fbe68eee94fe8d3150a3e37eff396cc5b7a55
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-16 01:14:42 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -3,6 +3,8 @@
 echo "Début d'installation des dépendances"
 sudo service mosquitto restart
 sudo adduser mosquitto
+sudo systemctl enable mosquitto
+sudo systemctl start mosquitto
 mosquitto -h | grep version > /var/www/html/plugins/worxLandroidS/versionmosquitto.php
 
 echo "Fin installation des dépendances"
