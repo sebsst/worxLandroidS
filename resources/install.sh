04f52f2242aadb24323baffa5e1114e70a359e29
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-15 23:35:26 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -8,8 +8,8 @@ sudo apt-key add mosquitto-repo.gpg.key
 rm mosquitto-repo.gpg.key
 cd /etc/apt/sources.list.d/
 sudo wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
-sudo apt-get update
-sudo apt-get install mosquitto mosquitto-clients
+sudo apt-get -y update
+sudo apt-get -y install mosquitto mosquitto-clients
 
 
 
