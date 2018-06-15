4f5f4f749923ecc65a9122d92da08aeee5c5f583
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-15 23:30:11 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -2,13 +2,12 @@
 
 echo "Début d'installation des dépendances"
 wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-apt-key add mosquitto-repo.gpg.key
+sudo apt-key add mosquitto-repo.gpg.key
+rm mosquitto-repo.gpg.key
 cd /etc/apt/sources.list.d/
-wget http://repo.mosquitto.org/debian/mosquitto-jessie.list
+sudo wget http://repo.mosquitto.org/debian/mosquitto-wheezy.list
 sudo apt-get update
-apt-get -y install mosquitto
-apt-get -y install mosquitto mosquitto-clients
-
+sudo apt-get install mosquitto mosquitto-clients
 
 
 
