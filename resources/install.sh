91b6348b26d347e9aaec248fb70fcdad498f86c3
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-16 00:59:06 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -1,16 +1,25 @@
 #! /bin/bash
 
 echo "Début d'installation des dépendances"
-sudo apt-get -y autoremove
-sudo apt-get -y dist-upgrade
+sudo apt-get -y install build-essential python quilt devscripts python-setuptools python3 libssl-dev cmake libc-ares-dev uuid-dev daemon
 
-wget http://repo.mosquitto.org/debian/mosquitto-repo.gpg.key
-sudo apt-key add mosquitto-repo.gpg.key
-rm mosquitto-repo.gpg.key
+wget https://libwebsockets.org/git/libwebsockets/snapshot/libwebsockets-1.4-chrome43-firefox-36.tar.gz
+tar zxvf libwebsockets-1.4-chrome43-firefox-36.tar.gz
+cd libwebsockets-1.4-chrome43-firefox-36
+mkdir build
+cd build
+sudo apt-get install zlibc zlib1g zlib1g-dev
+cmake ..
+sudo make install
+sudo ldconfig
+cd
 
-cd /etc/apt/sources.list.d/
-sudo wget http://repo.mosquitto.org/debian/mosquitto-wheezy.list
-sudo apt-get -y update
+wget http://mosquitto.org/files/source/mosquitto-1.4.2.tar.gz
+tar zxvf mosquitto-1.4.2.tar.gz
+cd mosquitto-1.4.2
 
-sudo apt-get -y install mosquitto mosquitto-clients
+make
+sudo make install
+sudo cp mosquitto.conf /etc/mosquitto
+
 echo "Fin installation des dépendances"
