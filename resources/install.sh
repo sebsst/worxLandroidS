e114b4f2591537cb1b0bf6118096ea9c27100dce
sebsst <sebastien.siegrist@yahoo.fr>
2018-06-16 22:09:46 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -29,13 +29,38 @@ echo 10 > /tmp/worxLandroidS_dep
 apt-get update
 echo 30 > /tmp/worxLandroidS_dep
 apt-get -y install mosquitto mosquitto-clients libmosquitto-dev
-echo 60 > /tmp/mqtt_dep
+
+#si version est toujours 1.3 alors on essaye de compiler une version plus récente
+mosquitto -h | grep "version"
+version=`mosquitto -h | grep "version 1.3"`
+if [ -n "$version" ]; then
+ if [ `lsb_release -i -s` == "Debian" ] || [ `lsb_release -i -s` == "Raspian" ]; then
+
+#   #  sudo apt-get -y install build-essential python quilt devscripts python-setuptools python3 libssl-dev cmake libc-ares-dev uuid-dev daemon
+     echo "La version de mosquitto $version n'est pas compatible. tentative d'installation d'une version plus récente"
+     sudo apt-get -y install cmake libssl1.0-dev 
+     sudo apt-get -y install libwebsockets-dev uuid-dev
+     cd /tmp
+     wget http://mosquitto.org/files/source/mosquitto-1.5.tar.gz
+     tar xavf mosquitto-1.5.tar.gz
+     cd mosquitto-1.5
+     cmake -DWITH_WEBSOCKETS=YES .
+     make -j4
+     sudo make install
+     apt-get -y install mosquitto mosquitto-clients
+     service mosquitto restart
+  
+
+  fi
+ fi
+
+echo 60 > /tmp/worxLandroidS_dep
 
 if [[ -d "/etc/php5/" ]]; then
   apt-get -y install php5-dev
   if [[ -d "/etc/php5/cli/" && ! `cat /etc/php5/cli/php.ini | grep "mosquitto"` ]]; then
   	echo "" | pecl install Mosquitto-alpha
-    echo 80 > /tmp/mqtt_dep
+    echo 80 > /tmp/worxLandroidS_dep
   	echo "extension=mosquitto.so" | tee -a /etc/php5/cli/php.ini
   fi
   if [[ -d "/etc/php5/fpm/" && ! `cat /etc/php5/fpm/php.ini | grep "mosquitto"` ]]; then
@@ -44,7 +69,7 @@ if [[ -d "/etc/php5/" ]]; then
   fi
   if [[ -d "/etc/php5/apache2/" && ! `cat /etc/php5/apache2/php.ini | grep "mosquitto"` ]]; then
   	echo "extension=mosquitto.so" | tee -a /etc/php5/apache2/php.ini
-    rm /tmp/mqtt_dep
+    rm /tmp/worxLandroidS_dep
     echo "Fin installation des dépendances"
     service apache2 restart
   fi
@@ -52,7 +77,7 @@ else
   apt-get -y install php7.0-dev
   if [[ -d "/etc/php/7.0/cli/" && ! `cat /etc/php/7.0/cli/php.ini | grep "mosquitto"` ]]; then
     echo "" | pecl install Mosquitto-alpha
-    echo 80 > /tmp/mqtt_dep
+    echo 80 > /tmp/worxLandroidS_dep
     echo "extension=mosquitto.so" | tee -a /etc/php/7.0/cli/php.ini
   fi
   if [[ -d "/etc/php/7.0/fpm/" && ! `cat /etc/php/7.0/fpm/php.ini | grep "mosquitto"` ]]; then
