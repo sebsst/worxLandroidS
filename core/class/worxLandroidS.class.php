2cb7c65acd240f01d652cfd846acd265e331e4bc
sebsst <sebastien.siegrist@yahoo.fr>
2018-07-08 22:24:54 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -669,16 +669,16 @@ schedule: TimePeriod[];
         self::newInfo($elogic,'lastTime',$json2_data->cfg->tm,'string',1);
 
         self::newInfo($elogic,'firmware',$json2_data->dat->fw,'string',0);
-        self::newInfo($elogic,'wifiQuality',$json2_data->dat->rsi,'string',0);
-        self::newInfo($elogic,'rainDelay',$json2_data->cfg->rd,'string',1);
+        self::newInfo($elogic,'wifiQuality',$json2_data->dat->rsi,'numeric',0);
+        self::newInfo($elogic,'rainDelay',$json2_data->cfg->rd,'numeric',1);
 
         self::newInfo($elogic,'totalTime',$json2_data->dat->st->wt,'numeric',1);
         self::newInfo($elogic,'totalDistance',$json2_data->dat->st->d,'numeric',1);
-        self::newInfo($elogic,'totalBladeTime',$json2_data->dat->st->b,'string',0);
+        self::newInfo($elogic,'totalBladeTime',$json2_data->dat->st->b,'numeric',0);
         self::newInfo($elogic,'batteryChargeCycle',$json2_data->dat->bt->nr,'numeric',1);
         self::newInfo($elogic,'batteryCharging',$json2_data->dat->bt->c,'binary',1);
-        self::newInfo($elogic,'batteryVoltage',$json2_data->dat->bt->v,'string',0);
-        self::newInfo($elogic,'batteryTemperature',$json2_data->dat->bt->t,'string',0);
+        self::newInfo($elogic,'batteryVoltage',$json2_data->dat->bt->v,'numeric',0);
+        self::newInfo($elogic,'batteryTemperature',$json2_data->dat->bt->t,'numeric',0);
         self::newInfo($elogic,'zonesList',$json2_data->dat->mz,'string',0);
 	//log::add('worxLandroidS', 'Debug', 'zone:' . $json2_data->cfg->mzv[$json2_data->dat->lz]+1 . ' / '.$json2_data->cfg->mz[1]);    
 //	if ($json2_data->cfg->mz[1] != 0){
