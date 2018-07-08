32a1dfb797c46b8d7a09b7aeb45dce3e629fb808
sebsst <sebastien.siegrist@yahoo.fr>
2018-07-08 16:57:03 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -454,6 +454,8 @@ class worxLandroidS extends eqLogic {
 
   public static function disconnect( $r ) {
     log::add('worxLandroidS', 'debug', 'Déconnexion de Mosquitto avec code ' . $r);
+	  
+    self::newInfo($elogic,'LastMosquittoCode', $r,'numeric',1);	  
     if($r == '14'){
 	  message::add('worxLandroidS', "Vous devez mettre à jour Mosquitto (version minimum 1.4 requise)");	    
     }
