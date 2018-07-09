a1ef8362df122bb8b491559469ceff774f5928fc
sebsst <sebastien.siegrist@yahoo.fr>
2018-07-09 17:39:06 +0200

    Update worxLandroidS.php

--- desktop/php/worxLandroidS.php
+++ desktop/php/worxLandroidS.php
@@ -123,7 +123,6 @@ $eqLogics = eqLogic::byType('worxLandroidS');
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isEnable" checked/>{{Activer}}</label>
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="isVisible" checked/>{{Visible}}</label>
                 <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="errorRetryMode" checked/>{{retry Mode}}</label>
-                <label class="checkbox-inline"><input type="checkbox" class="eqLogicAttr" data-l1key="automaticWidget" checked/>{{automatic Widget}}</label>
 
               </div>
             </div>
