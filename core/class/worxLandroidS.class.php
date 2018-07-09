aa27d4a0adf650937adc2c7fdefec266ea538aa1
sebsst <sebastien.siegrist@yahoo.fr>
2018-07-09 14:02:38 +0200

    Update worxLandroidS.class.php

--- core/class/worxLandroidS.class.php
+++ core/class/worxLandroidS.class.php
@@ -1154,7 +1154,7 @@ public static $_widgetPossibility = array('custom' => array(
 
 	    }   
 		
-            if(	(strpos($cmd->getId() , 'Planning') !== 0){	
+            if(	(strpos($cmd->getId() , 'Planning') === 0){} else {	
             $cmd_html .= $cmd->toHtml($_version, '', $replace['#cmd-background-color#']);}
             	
             if ($cmd->getIsHistorized() == 1) {
