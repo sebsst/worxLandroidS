1a94fb5f6a2acbe4da74abd79ca62d4bc40fe233
sebsst <sebastien.siegrist@yahoo.fr>
2018-09-28 23:12:35 +0200

    Update install.sh

--- resources/install.sh
+++ resources/install.sh
@@ -59,33 +59,6 @@ else
 fi
 echo 60 > ${PROGRESS_FILE}
 
-#si version est toujours 1.3 alors on essaye de compiler une version plus récente
-mosquitto -h | grep "version"
-version=`mosquitto -h | grep "version 1.3"`
-if [ -n "$version" ]; then
- if [ `lsb_release -i -s` == "Debian" ] || [ `lsb_release -i -s` == "Raspian" ]; then
-
-#   #  sudo apt-get -y install build-essential python quilt devscripts python-setuptools python3 libssl-dev cmake libc-ares-dev uuid-dev daemon
-     echo "La version de mosquitto $version n'est pas compatible. tentative d'installation d'une version plus récente"
-     sudo apt-get -y install cmake libssl1.0-dev 
-     sudo apt-get -y install libwebsockets-dev uuid-dev
-     cd /tmp
-     wget http://mosquitto.org/files/source/mosquitto-1.5.tar.gz
-     tar xavf mosquitto-1.5.tar.gz
-     cd mosquitto-1.5
-     cmake -DWITH_WEBSOCKETS=YES .
-     make -j4
-     sudo make install
-     apt-get -y install mosquitto mosquitto-clients
-     service mosquitto restart
-  
-
-  fi
- fi
-
-
-
-
 echo "*"
 echo "* Install php mosquitto wrapper"
 echo "*"
